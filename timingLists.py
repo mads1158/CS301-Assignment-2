50e78c66222a5c7e7df8b8ac82ac2082c9772baf
Jackson Madsen <mads1158@bears.unco.edu>
2025-01-30 12:13:13 -0700

    all tests & data

--- timingLists.py
+++ timingLists.py
@@ -3,7 +3,7 @@ from time import perf_counter
 fOut = open("times.csv", "w+")
 
 #Create the header for the file
-fOut.write(" , Add to Front, Add to End, Add to Middle, Remove from Front, Remove from Middle, Remove from End\r\n")
+fOut.write(" , Add to Front, Add to End, Add to Middle, Remove from Front, Remove from Middle, Remove from End, Search Worst-Case\r\n")
 
 def ListTests(demoList):
     #List add to front
@@ -44,7 +44,14 @@ def ListTests(demoList):
     t5 = t5Stop - t5Start
     t6 = t6Stop - t6Start
 
-    fOut.write(f"{t1},{t2},{t3},{t4},{t5},{t6}\r\n")
+    #search worst-case
+    t7Start = perf_counter()
+    toss = "DNE" in demoList
+    t7Stop = perf_counter()
+    t7 = t7Stop - t7Start
+        
+
+    fOut.write(f"{t1},{t2},{t3},{t4},{t5},{t6},{t7}\r\n")
 
 def dictionaryTests(demoDic):
     #add to dictionary
@@ -58,6 +65,14 @@ def dictionaryTests(demoDic):
     demoDic["b"] = "B"
     t2Stop = perf_counter()
     t2 = t2Stop - t2Start
+
+    #Worst Case Search
+    t3Start = perf_counter()
+    toss = "DNE" in demoDic
+    t3Stop = perf_counter()
+    t3 = t3Stop - t3Start
+
+    fOut.write(f"{t1},n/a,n/a,{t2},n/a,n/a,{t3}\r\n")
 
 
 
@@ -84,6 +99,27 @@ for x in range(10**8):
     largeList.append(x)
 
 ListTests(largeList)
+
+#small dictionary
+fOut.write("Small Dictionary,")
+smallDict = {}
+for x in range(10**3):
+    smallDict[x] = x
+dictionaryTests(smallDict)
+
+#medium dictionary
+fOut.write("Medium Dictionary,")
+mediumDict = {}
+for x in range(10**5):
+    mediumDict[x] = x
+dictionaryTests(mediumDict)
+
+#large dictionary
+fOut.write("Large Dictionary,")
+largeDict = {}
+for x in range(10**8):
+    largeDict[x] = x
+dictionaryTests(largeDict)
 
 fOut.close()
 
